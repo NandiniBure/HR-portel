78ceaba301854bcfe088e26dd59b293b2e5f887c
NandiniBure <nandinibure@gmail.com>
2026-04-16 16:10:28 +0530

    fixes for deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-import app from "../backend/src/app.js";
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const { default: app } = await import("../backend/src/app.js");
   return app(req, res);
 }
