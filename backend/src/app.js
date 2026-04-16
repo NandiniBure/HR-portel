dae1c96844aa5000b1450caae9897574100db5b2
NandiniBure <nandinibure@gmail.com>
2026-04-16 15:36:50 +0530

    fixes for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
 import employeeRoutes from "./routes/employeeRoute.js";
 import authRoutes from "./routes/authRoute.js";
 import leavesRoute from "./routes/leaveRoute.js";
@@ -10,34 +12,38 @@ import payrollRoutes from "./routes/payrollRoute.js";
 import departmentRoutes from "./routes/departmentRoute.js";
 import designationRoutes from "./routes/designationRoute.js";
 import masterRoutes from "./routes/masterRoutes.js";
-import cookieParser from "cookie-parser";
+
 const app = express();
 
-/* ---------------- Middleware ---------------- */
+app.set("trust proxy", 1);
 
 app.use(express.json());
+
 app.use(
   cors({
-    origin: "http://localhost:8080", // 🔁 change to your frontend URL
+    origin: true,
     credentials: true,
   })
 );
+
+app.use(cors());
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
   })
 );
+
 app.use(morgan("dev"));
-app.use(cookieParser()); // ✅ REQUIRED
+app.use(cookieParser());
+
 app.use("/api/payroll", payrollRoutes);
 app.use("/api", attendanceRoute);
 app.use("/api", leavesRoute);
 app.use("/api", employeeRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/departments", departmentRoutes);
-app.use("/designations", designationRoutes);
+app.use("/api/departments", departmentRoutes);
+app.use("/api/designations", designationRoutes);
 app.use("/api", masterRoutes);
-/* ---------------- Health Check ---------------- */
 
 app.get("/", (req, res) => {
   res.status(200).json({
