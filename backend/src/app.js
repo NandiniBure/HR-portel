f5d62f755f4402978c2ee0f933fd4780fd42bc1d
NandiniBure <nandinibure@gmail.com>
2026-05-05 23:31:25 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://hr-portel-uaew.vercel.app/", "http://localhost:8080"],
+    origin: ["https://hr-portel-uaew.vercel.app", "http://localhost:8080"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
